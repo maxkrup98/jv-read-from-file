da259330b4b13b855119d56017f96ec0281d9858
Max Krup <tochnonemaks@gmail.com>
2025-03-20 21:16:48 +0200

    add solution

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,58 @@
 package core.basesyntax;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class FileWork {
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        int wordCntr = 0;
+        try (BufferedReader countReader = new BufferedReader(new FileReader(fileName))) {
+
+            String line;
+            while ((line = countReader.readLine()) != null) {
+                String resultLine = line.replaceAll("[^a-zA-Z0-9]", " ");
+                String[] words = resultLine.split(" ");
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    if (word.startsWith("w")) {
+                        wordCntr++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            return new String[0];
+        }
+        if (wordCntr == 0) {
+            return new String[0];
+        }
+        String[] wordsStartsW = new String[wordCntr];
+        int index = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String resultLine = line.replaceAll("[^a-zA-Z0-9]", " ");
+                String[] words = resultLine.split(" ");
+                for (String word : words) {
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    if (word.startsWith("w")) {
+                        wordsStartsW[index] = word;
+                        index++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            return new String[0];
+        }
+        Arrays.sort(wordsStartsW);
+        return wordsStartsW;
     }
 }
+
